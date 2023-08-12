6c53147605cb14172d8265d6bd90923c7b4235f1
IZABELA TAYNÁ REIS COIMBRA <1091392313038@fatec-srv.local>
2023-08-11 21:58:51 -0300

    (11/08) Funções

---
+++ 01_funcoes.py
@@ -0,0 +1,12 @@
+# função para calcular IMC
+def calc_imc(peso, altura):
+     return peso / altura ** 2
+
+#maneira comum
+# p = 85
+# a = 1.72
+# imc = calc_imc(p, a)
+# print(imc) 
+
+print(calc_imc(85, 1.72)) #maneira direta
+
